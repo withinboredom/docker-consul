3cc016405f1d0bb91ebbc55f929bfa11fd6b3512
Rob Landers <landers.robert@gmail.com>
2015-06-13 03:48:18 -0400

    updated builds/puhses

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-docker build -t withinboredom/consul consul
+docker tag -f withinboredom/ubuntu-base withinboredom/consul:base
+docker build -t withinboredom/consul:latest consul
 #docker push withinboredom/consul
